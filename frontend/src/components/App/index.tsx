4abc8abae5664c02977212ce39ed6beba6f93818
Мальцев Роман <mra001@krasintegra.ru>
2024-04-05 16:55:56 +0700

    started

---
+++ frontend/src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a className="logo" href="/">
+          <img src="/images/logo.png" alt="logo" />
+        </a>
+        <h2 className="fullName">Казанце Захар</h2>
+        <button className="create-btn">Создать работу</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
